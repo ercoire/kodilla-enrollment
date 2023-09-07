13c4994039d4d4ace86a1e3798d559b83e86a535
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-09-08 00:14:41 +0200

    First commit

---
+++ src/test/java/com/example/kodillaenrollment/KodillaEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodillaenrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaEnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
